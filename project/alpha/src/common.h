220e8b6905b35d7d08cea08c076d837ec275e1d1
penguin <kaishansheng@163.com>
2017-06-29 11:30:36 +0800

    add server alpha

--- project/alpha/src/common.h
+++ project/alpha/src/common.h
@@ -12,6 +12,8 @@ ericsheng     2016.4.13   1.0     Create
 #ifndef __COMMON_H__
 #define __COMMON_H__
 
+#include "base/trace.h"
+#include "base/dictionary.h"
 #include <string>
 namespace serverframe
 {
@@ -26,13 +28,33 @@ enum MANAGER_ERROR
     MANAGER_E_SERVER_NOTIFY_DATA_INVALID = 301005,
 };
 
+enum ATP_MESSAGE_TYPE
+{
+    ATP_MESSAGE_TYPE_INVALID = -1,
+    ATP_MESSAGE_TYPE_QUERY_REQ = 1,
+    ATP_MESSAGE_TYPE_TRADE_RSP = 2,
+};
+
+
+/* atp message command defines */
+const char* const ATPM_CMD_ERROR = "cmd_error";
+const char* const ATPM_CMD_ENTRUST = "entrust";
+const char* const ATPM_CMD_WITHDRAW = "withdraw";
+const char* const ATPM_CMD_ENTRUST_RESULT = "entrust_result";
+const char* const ATPM_CMD_DEAL_RESULT = "deal_result";
+const char* const ATPM_CMD_WITHDRAW_RESULT = "withdraw_result";
+const char* const ATPM_CMD_QRY_DEAL = "qry_deal";
+const char* const ATPM_CMD_RSP_DEAL = "rsp_deal";
+const char* const ATPM_CMD_QRY_ENTRUST = "qry_entrust";
+const char* const ATPM_CMD_RSP_ENTRUST = "rsp_entrust";
+
 //expireinst_flag
 const char EXPIREINST_FLAG_ALL = '0';
 const char EXPIREINST_FLAG_SHFE = '1';
 const char EXPIREINST_FLAG_CFFEX = '2';
 
-const char *const MODULE_NAME = "delta";
-const int MANAGER_S_OK = 0;
+const char *const MODULE_NAME = "alpha";
+const int NAUT_S_OK = 0;
 
 #define CHECK_IF_DBCONN_NULL(dbconn) \
 if(dbconn == NULL){ \
@@ -62,6 +84,7 @@ if(!dbconn->_conn->rollback()){ \
     return MANAGER_E_DB_FAILED;\
 } \
 
+typedef base::ref_adapter<base::dictionary> ref_dictionary;
 }
 
 #endif
---
+++ project/alpha/src/product_business_deal.cpp
@@ -0,0 +1,60 @@
+/***************************************************************************** 
+a50-zd Module Copyright (c) 2015. All Rights Reserved. 
+FileName: product_business_deal.cpp
+Version: 1.0 
+Date: 2015.10.26 
+History: cwm     2015.10.26   1.0     Create 
+******************************************************************************/
+
+#include "product_business_deal.h"
+#include "process.h"
+#include "base/trace.h"
+#include <math.h>
+
+namespace serverframe
+{
+
+product_business_deal::product_business_deal()
+{
+    
+}
+
+product_business_deal::~product_business_deal()
+{
+    
+}
+
+int product_business_deal::check_deal_is_exist(base::dictionary &dict)
+{
+    int ret = NAUT_S_OK;
+
+    LABEL_SCOPE_START;
+
+
+    LABEL_SCOPE_END;
+end:
+    return ret;
+}
+
+int product_business_deal::get_stocker_attr(base::dictionary &d,const std::string &user_name)
+{
+    int ret = NAUT_S_OK;
+    LABEL_SCOPE_START;
+
+    LABEL_SCOPE_END;
+
+    return ret;
+}
+
+int product_business_deal::add_deal(base::dictionary &dict, database::db_instance* pconn)
+{
+    int ret = NAUT_S_OK;
+    LABEL_SCOPE_START;
+
+    LABEL_SCOPE_END;
+
+end:
+    return ret;
+}
+
+}
